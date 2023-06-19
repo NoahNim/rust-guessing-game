4af2566adb2e0920bb9e4b73c640d26632ba8490
Noah Medoff <noahmedoff@gmail.com>
2023-06-19 13:13:30 -0700

    follow along to code in book and start comments on that code for learning

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,15 @@
+use std::io; //puts this std library for Rust into this file
+
 fn main() {
-    println!("Hello, world!");
+    println!("Guess the number");
+
+    println!("Please input your game.");
+
+    let mut guess = String::new();
+
+    io::stdin()
+        .read_line(&mut guess)
+        .expect("Failed to read line");
+
+    println!("You guessed: {guess}");
 }

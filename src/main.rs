3c0795a5d8865ff757bb5f6cd1693fa38b5744a1
Noah Medoff <noahmedoff@gmail.com>
2023-06-19 17:21:22 -0700

    commented on the new code and removed it printing the secret number

--- src/main.rs
+++ src/main.rs
@@ -1,13 +1,11 @@
-use rand::Rng;
-use std::{cmp::Ordering, io}; //puts this std input/ouput library for Rust into this file
+use rand::Rng; // Import the `Rng` trait from the `rand` crate
+use std::{cmp::Ordering, io}; //Import this std input/ouput library for Rust into this file
 
 fn main() {
     // In Rust, fn main() is the entry point of a Rust program. It is the starting point of execution when the program is run. The main function is where the program's execution begins and it is mandatory in every Rust executable.
     println!("Guess the number!"); // Print the string "Guess the number" to the console
 
     let secret_number = rand::thread_rng().gen_range(1..=100); // generates a secret number between 1 and 100 using the rand library
-
-    println!("The secret number is: {secret_number}"); // prints the secret number for development
 
     loop {
         println!("Please input your guess."); // Print the string "Please input your guess." to the console
@@ -20,8 +18,8 @@ fn main() {
 
         let guess: u32 = match guess.trim().parse() {
             // Parse the input string `guess` into an unsigned 32-bit integer (`u32`)
-            Ok(num) => num,
-            Err(_) => continue,
+            Ok(num) => num, // If parsing is successful, assign the parsed value to `guess`
+            Err(_) => continue, // If parsing fails, continue to the next iteration of the loop
         };
 
         println!("You guessed: {guess}"); // Print the string "You guessed: " followed by the value of `guess` to the console
